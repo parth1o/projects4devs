2b09085e14a90e77714a8856dabea299954d3a93
Parth Patel <ppat159@aucklanduni.ac.nz>
2021-07-03 02:39:55 +1200

    test

--- projects4devs/src/App.js
+++ projects4devs/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <p className="desc">
         projects4devs aims to solve the issue student developers have in finding meaningful projects. Students are able to become project owners and put their project ideas onto the site and other students can request to join their project, if the project owner sees the requested students profile and sees them as a goodfit then they will be able approve the student to come in on the project. project4devs helps to add an additional layer of support for student devs as through these projects they will be able to gain valuable experience which will help them to get that dream internship or job. 
       </p>
-      <Button variant="contained">Logi</Button>
+      <Button variant="contained">Login</Button>
       <Button variant="contained">Sign Up</Button>
     </div>
     </html>
