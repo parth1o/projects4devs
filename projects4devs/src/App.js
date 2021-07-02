1d3a9a2811fa3356c459fb5406a557c14321d58d
Parth Patel <ppat159@aucklanduni.ac.nz>
2021-07-03 02:56:41 +1200

    test

--- projects4devs/src/App.js
+++ projects4devs/src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <html>
     <div className="App">
-      <h1 className="title">projects4devs</h1>
+      <h1 className="title">projects<span className="single">4</span>devs</h1>
       <p className="desc">
-        projects4devs aims to solve the issue student developers have in finding meaningful projects. Students are able to become project owners and put their project ideas onto the site and other students can request to join their project, if the project owner sees the requested students profile and sees them as a goodfit then they will be able approve the student to come in on the project. project4devs helps to add an additional layer of support for student devs as through these projects they will be able to gain valuable experience which will help them to get that dream internship or job. 
+        projects4devs aims to solve the issue student developers have in finding meaningful projects.Students are able to become project owners and put their project ideas onto the site and other students can request to join their project,if the project owner sees the requested students profile and sees them as a goodfit then they will be able approve the student to come in on the project.project4devs helps to add an additional layer of support for student devs as through these projects they will be able to gain valuable experience which will help them to get that dream internship or job. 
       </p>
         <Button className="homebuttons" variant="contained">Login</Button>
         <Button className="homebuttons" variant="contained">Sign Up</Button>
