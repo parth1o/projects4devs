65cf34b63df655fde045b61b822032b92785d177
Justin <justinteo1997@hotmail.co.uk>
2021-07-03 20:10:52 +1200

    integrate create project with backend

--- frontend/src/headerbar.js
+++ frontend/src/headerbar.js
@@ -1,28 +1,62 @@
-import {Link} from "react-router-dom";
-import React from "react";
-import {Button} from "@material-ui/core";
-import "./headerbar.css";
+import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
+import './headerbar.css';
+import Dialog from '@material-ui/core/Dialog';
+import CreateProject from "./createproject";
 
-function headerbar(){
+
+function Headerbar() {
+    const [open, setOpen] = useState(false);
+
+    function openModal() {
+        setOpen(true)
+    }
+
+    function closeModal() {
+        setOpen(false)
+        window.location.reload();
+    }
+
     return (
-        <nav className="navbar navbar navbar-default navbar-expand-sm">
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a>
-                        <Link className="nav-item nav-link" to='/profile'>Profile</Link>
-                    </a>
-                    <a>
-                        <Link className="nav-item nav-link" to='/dashboard'>Dashboard</Link>
-                    </a>
-                    <a>
-                        <Link className="nav-item nav-link" to='/dashboard'>Create Project</Link>
-                    </a>
-                    <a>
-                        <Link className="nav-item nav-link" to='/'>Log Out</Link>
-                    </a>
+        <div>
+            <nav className="navbar navbar navbar-default navbar-expand-sm">
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarNavAltMarkup"
+                >
+                    <div className="navbar-nav">
+                        <a>
+                            <Link className="nav-item nav-link" to="/profile">
+                                Profile
+                            </Link>
+                        </a>
+                        <a>
+                            <Link className="nav-item nav-link" to="/dashboard">
+                                Dashboard
+                            </Link>
+                        </a>
+                        <a>
+                            <Link
+                                className="nav-item nav-link"
+                                to="/dashboard"
+                                onClick={openModal}
+                            >
+                                Create Project
+                            </Link>
+                        </a>
+                        <a>
+                            <Link className="nav-item nav-link" to="/">
+                                Log Out
+                            </Link>
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <CreateProject close={closeModal}/>
+            </Dialog>
+        </div>
+    );
 }
-export default headerbar;
+export default Headerbar;
