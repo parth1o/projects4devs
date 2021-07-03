7b6dcf17f9b8372e914c8e75e02943f0ee027084
femmon <49446416+catanon@users.noreply.github.com>
2021-07-03 12:38:57 +1200

    Add retrieve route + error handling

--- backend/src/routes/api/user.js
+++ backend/src/routes/api/user.js
@@ -2,14 +2,28 @@ import express from 'express';
 import * as usersDao from '../../db/users-dao';
 
 const HTTP_CREATED = 201;
+const HTTP_BAD_REQUEST = 400;
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const newUser = await usersDao.createUser(req.body);
-    res.status(HTTP_CREATED)
-        .header('location', `/api/user/${newUser._id}`)
-        .json(newUser);
+    try {
+        const newUser = await usersDao.createUser(req.body);
+        res.status(HTTP_CREATED)
+            .header('location', `/api/user/${newUser._id}`)
+            .json(newUser);
+    } catch {
+        res.sendStatus(HTTP_BAD_REQUEST);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        let user = await usersDao.retrieveUser(req.params.id)
+        res.json(user);
+    } catch {
+        res.sendStatus(HTTP_BAD_REQUEST);
+    }
 });
 
 export default router;
