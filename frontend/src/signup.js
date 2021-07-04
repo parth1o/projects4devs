fc8d778bbb8c0d71259a6c17bdce30abb554d58d
michelle-wdcc <69370899+michelle-wdcc@users.noreply.github.com>
2021-07-04 12:44:50 +1200

    Add skill

--- frontend/src/signup.js
+++ frontend/src/signup.js
@@ -7,8 +7,22 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useHistory } from 'react-router-dom';
+import MenuItem from '@material-ui/core/MenuItem';
 
-
+const levels = [
+  {
+    value: 'Beginner',
+    label: 'Beginner',
+  },
+  {
+    value: 'Intermediate',
+    label: 'Intermediate',
+  },
+  {
+    value: 'Professional',
+    label: 'Professional',
+  },
+];
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -38,6 +52,9 @@ export default function SignUp() {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [language, setLanguage] = useState('');
+  const [level, setLevel] = useState('');
+  const [skills, setSkills] = useState([]);
   const [description, setDescription] = useState('');
 
   const signUp = async () => {
@@ -51,6 +68,8 @@ export default function SignUp() {
         lastName,
         email,
         password,
+        language,
+        level,
         description
       })
     });
@@ -140,6 +159,50 @@ export default function SignUp() {
                 value={password}
               />
             </Grid>
+            <Grid item xs={12}>
+              <TextField
+              InputLabelProps={{
+                style: { color: '#FFDBB5' },
+              }}
+                variant="outlined"
+                required
+                fullWidth
+                id="language"
+                label="Technical language you are familiar with"
+                name="language"
+                autoComplete="language"
+                onChange={event => setLanguage(event.target.value)}
+                value={language}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <p className="skill"> Please select level of proficiency</p>
+              <TextField
+              InputLabelProps={{
+                style: { color: '#FFDBB5' },
+              }}
+                variant="outlined"
+                required
+                fullWidth
+                select
+                name="level"
+                label="Level of Proficiency"
+                id="level"
+                value={level}
+                onChange={event => setLevel(event.target.value)}
+              
+              >
+                {levels.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Button className="addbutton" onClick={() => setSkills([...skills, {language, level}])}>Add</Button>
+            <div >{skills.map(skill => {
+              return <span className="skill" >{skill.language} - {skill.level}</span>
+              })}</div>
             <Grid item xs={12}>
               <TextField
               InputLabelProps={{
